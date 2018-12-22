e6baa87946c7516549a006f0c00083a0f96a8dbc
devraf <rafael.perales@outlook.com>
2018-12-22 16:33:17 -0600

    updated tip app

--- tip/app.js
+++ tip/app.js
@@ -8,19 +8,22 @@ var app = new Vue({
   },
   watch: {
     total: function () {
+      //show/hide $ sign
       if(this.total != '') {
         this.notZero = true;
         console.log(this.total);
       } else this.notZero = false;
 
+      //scale tip amount by percent
+      this.tipRange = [];
       var scale = .1;
       var tipScaler = 0;
       for(var i = 0; i < 4; i++) {
-        tipScaler = this.total * scale;
-        this.tipRange.push(tipScaler);
+        tipScaler = (this.total * scale.toFixed(2));
+        this.tipRange.push(tipScaler.toFixed(2));
         scale += .05;
-        console.log(this.tipRange);
-      }
+      };
+      //Update the tip section
     }
   }
 
