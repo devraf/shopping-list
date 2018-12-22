dd09d7de9a7b753f609f27696563e978f47cefbb
devraf <rafael.perales@outlook.com>
2018-12-22 17:28:21 -0600

    completed version of tip app

--- tip/app.js
+++ tip/app.js
@@ -4,23 +4,33 @@ var app = new Vue({
     header: 'Tipper',
     total: '',
     notZero: false,
-    tipRange: []
+    tipRange: [],
+    totalPlusTip: []
   },
   watch: {
     total: function () {
       //show/hide $ sign
       if(this.total != '') {
         this.notZero = true;
-        console.log(this.total);
       } else this.notZero = false;
 
       //scale tip amount by percent
       this.tipRange = [];
+      this.totalPlusTip = [];
+
       var scale = .1;
       var tipScaler = 0;
+      var tipAndTotal = 0;
+
       for(var i = 0; i < 4; i++) {
+        //Update tip amount and add to tipRange array
         tipScaler = (this.total * scale.toFixed(2));
         this.tipRange.push(tipScaler.toFixed(2));
+
+        //Update total plus tip and add to totalPlusTip array
+        tipAndTotal = Number.parseFloat(this.tipRange[i]) + Number.parseFloat(this.total)
+        this.totalPlusTip.push(tipAndTotal.toFixed(2));
+
         scale += .05;
       };
       //Update the tip section
